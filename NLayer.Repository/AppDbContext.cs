47f5734be0509f8cd2ddfac27867ec829662b1b8
Serhat OMAÇ <omacserhatt@gmail.com>
2022-06-19 21:57:51 +0300

    AppDbContext+EntityConfiguration+SeedData eklendi.

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        //Samet abiye sor.
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Assemblydeki tüm interfaceleri bulsun ve uygulasın.
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.Entity<ProductFeature>().HasData(
+                new ProductFeature()
+                {
+                    Id = 1,
+                    Color = "Kırmızı",
+                    Height = 100,
+                    Width = 200,
+                    ProductId = 1
+                },
+                new ProductFeature()
+                {
+                    Id = 2,
+                    Color = "Mavi",
+                    Height = 300,
+                    Width = 500,
+                    ProductId = 2
+                });
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
+
---
+++ NLayer.Repository/Seeds/CategorySeed.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    //Seed Data her projede ihtiyaç olan bir konu değildir.
+    //Özellikle multi-tenant uygulamalarda ihtiyaç olur.
+    //Müşteriye ait bir db oluşturulurken aynı zamanda default rol bilgilerinin veya default kullanıcıların bilgilerinin db'ye yazılması istenebilir.
+    //Özetle her zaman kullanılmaz, ihtiyaç olduğunda.
+    internal class CategorySeed : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(
+                new Category { Id = 1, Name = "Kalemler" },
+                new Category { Id = 2, Name = "Kitaplar" },
+                new Category { Id = 3, Name = "Defterler" }
+                );
+        }
+    }
+}
